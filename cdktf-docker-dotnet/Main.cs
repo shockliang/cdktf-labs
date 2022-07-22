dd3e153d8e70ffe22f2798d2527aedcd8d0aa65e
shockliang <shockwater@gmail.com>
2022-07-23 00:31:47 +0800

    Docker provider of dotnet.

---
+++ cdktf-docker-dotnet/Main.cs
@@ -0,0 +1,43 @@
+using System;
+using Constructs;
+using HashiCorp.Cdktf;
+
+using HashiCorp.Cdktf.Providers.Docker;
+
+
+namespace MyCompany.MyApp
+{
+    class MyApp : TerraformStack
+    {
+        public MyApp(Construct scope, string id) : base(scope, id)
+        {
+
+            new DockerProvider(this, "docker", new DockerProviderConfig { });
+
+            var dockerImage = new Image(this, "nginxImage", new ImageConfig
+            {
+                Name = "nginx:latest",
+                KeepLocally = false,
+            });
+
+            new Container(this, "nginxContainer", new ContainerConfig
+            {
+                Image = dockerImage.Latest,
+                Name = "tutorial",
+                Ports = new [] { new ContainerPorts
+                {
+                    Internal = 80,
+                    External = 8000
+                }}
+            });
+        }
+
+        public static void Main(string[] args)
+        {
+            App app = new App();
+            new MyApp(app, "cdktf-docker-dotnet");
+            app.Synth();
+            Console.WriteLine("App synth complete");
+        }
+    }
+}
